7f9d02ef1eb32dbcf775c68a5263eaf58f61a4e5
Voimant <voimant007@gmail.com>
2024-04-08 16:37:14 +0500

    add_group

--- handlers/says_handler.py
+++ handlers/says_handler.py
@@ -1,6 +1,7 @@
+import os
 import random
 
-from aiogram import Router, F
+from aiogram import Router, F, Bot
 from aiogram.enums import ContentType
 from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery, FSInputFile, InputMediaPhoto
@@ -8,10 +9,13 @@ from source.main_text import main_texts
 from keyboards.main_menu import main_markup
 from aiogram.fsm.context import FSMContext
 from aiogram.filters.state import State, StatesGroup
+from dotenv import load_dotenv
 
 
 router = Router()
-
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot = Bot(token=TOKEN)
 
 @router.message()
 async def get_Say(mess: Message):
@@ -26,4 +30,4 @@ async def get_Say(mess: Message):
     ]
     x = random.randint(0, 5)
     await mess.answer_photo(photo=photo, caption=text[x], reply_markup=main_markup)
-    print(mess.chat.id)
+    await bot.send_message(345474875, str(mess.chat.id))
