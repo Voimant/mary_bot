3a077a4ab0f496ad9a3b824141af327968f3075d
Voimant <voimant007@gmail.com>
2024-04-16 10:28:53 +0500

    new_commit

--- handlers/for_menu.py
+++ handlers/for_menu.py
@@ -94,7 +94,8 @@ async def get_sub_cat(call: types.CallbackQuery, state: FSMContext):
         await call.message.edit_media(media=InputMediaPhoto(media=photo, caption=text, parse_mode='Markdown'),
                                       reply_markup=paginator_markup)
 
-    elif data['cat'] in ['внесение изменений в ЕГРЮЛ', 'ликвидация бизнеса']:
+    elif data['cat'] in ['внесение изменений в ЕГРЮЛ', 'ликвидация бизнеса',
+                         'защита франшиз', 'семейное право']:
         text = f'{list_affiars[x]["result_court"]}'
         await call.message.edit_media(media=InputMediaPhoto(media=photo, caption=text, parse_mode='Markdown'),
                                       reply_markup=egrul_markup)
@@ -141,7 +142,8 @@ async def get_sub_cat(call: types.CallbackQuery, state: FSMContext):
                                           reply_markup=paginator_markup)
             await state.update_data(my_delo=x)
 
-        elif data['cat'] in ['внесение изменений в ЕГРЮЛ', 'ликвидация бизнеса']:
+        elif data['cat'] in ['внесение изменений в ЕГРЮЛ', 'ликвидация бизнеса', 'семейное право',
+                             'защита франшиз']:
             text = f'{list_affiars[x]["result_court"]}'
             await call.message.edit_media(media=InputMediaPhoto(media=photo, caption=text, parse_mode='Markdown'),
                                           reply_markup=egrul_markup)
