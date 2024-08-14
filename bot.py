e62e384f2c0b75e81118f90e48ad08de1fec06bf
root <root@cv3694761.novalocal>
2024-08-14 12:42:15 +0300

    new up

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from config import TOKEN
+import os
+from dotenv import load_dotenv
 from handlers import main_handlers, for_menu, says_handler, sovets
 import logging
+load_dotenv()
 
 
 async def main():
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=os.getenv('TOKEN'))
     dp = Dispatcher()
     dp.include_routers(main_handlers.router, for_menu.router, says_handler.router, sovets.router)
     logging.basicConfig(level=logging.INFO)
